5f7f97a35eae8dd4ed635dd90baa5320ad452b8a
Ahmed Rafiq Ullah <ahmedrafiq47@gmail.com>
2021-04-18 18:52:19 +0600

    Create basic endpoints

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,47 @@
 
 #[macro_use]
 extern crate rocket;
+extern crate rocket_contrib;
+extern crate serde;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello World"
+use rocket_contrib::json::Json;
+use serde::Serialize;
+
+static TRANSACTIONS: [Transaction; 2] = [
+    Transaction::new(1, "Misc", "Bob", None),
+    Transaction::new(2, "Misc", "Alice", None)
+];
+
+#[derive(Serialize,Clone)]
+struct Transaction<'a> {
+    id: i32,
+    category: &'a str,
+    transactee: &'a str,
+    note: Option<&'a str>,
+}
+
+impl Transaction<'_> {
+    const fn new<'a>(id: i32, category: &'a str, transactee: &'a str, note: Option<&'a str>) -> Transaction<'a> {
+        Transaction {
+            id,
+            category,
+            transactee,
+            note,
+        }
+    }
+}
+
+#[get("/transactions/<id>")]
+fn get_transaction(id: i32) -> Option<Json<Transaction<'static>>> {
+    let transaction = TRANSACTIONS.iter().find(|transaction| transaction.id == id).cloned();
+    transaction.map(|transaction| Json(transaction))
+}
+
+#[get("/transactions")]
+fn get_all_transactions() -> Json<&'static [Transaction<'static>]> {
+    Json(&TRANSACTIONS)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![get_transaction, get_all_transactions]).launch();
 }
