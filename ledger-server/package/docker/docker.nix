db46428a9070c2ca966bd1f4228613a64f3e11ee
Ahmed Rafiq <ahmedrafiq47@gmail.com>
2025-01-05 16:41:02 +0600

    Create nix script to build docker image

---
+++ ledger-server/package/docker/docker.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  ledger = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "ledger";
+    version = "0.12.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "holahmeds";
+      repo = "ledger-server";
+      rev = "v${version}";
+      sha256 = "sha256-Dy/jTWLwLUY3sXgLyKMQeAWaWVKsvBn2w/NHg4y0J9Y=";
+    };
+
+    cargoHash = "sha256-QBoElC9x4Nt0I0+KAjURc8nfU4ibAVm/bseKGiQIQA4=";
+  };
+in
+pkgs.dockerTools.buildLayeredImage {
+  name = ledger.pname;
+  tag = ledger.version;
+
+  config.Cmd = [ "${ledger}/bin/ledger-server" ];
+}
